434c22d29556216320c712a86d46220d6fb13795
Nathan Kutzan <nkutzan@gmail.com>
2021-10-08 23:59:42 -0400

    Add stunned sound

--- ui/src/components/SoundPlayer/index.tsx
+++ ui/src/components/SoundPlayer/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { useState } from "react";
-import { useRoboticonGameStateSubscription } from "../../generated/graphql";
-import { GameState, GameType } from "../../roboticon";
+import { useRoboticonGameStateSubscription, useRoboticonStunnedAlliancesSubscription } from "../../generated/graphql";
+import { GameState, GameType, StunnedAlliances } from "../../roboticon";
 
 //const abort = new Audio('/audio/abort.wav');
 const end = new Audio('/audio/end.wav');
 //const resume = new Audio('/audio/resume.wav');
 const start = new Audio('/audio/start.wav');
 const warning = new Audio('/audio/warning.wav');
+const stunned = new Audio('/audio/stunned.mp3')
 
 export default function SoundPlayer() {
 
     const [previouslyEnabled, setPreviouslyEnabled] = useState(false)
     const [previousTime, setPreviousTime] = useState(0)
+
+    const [redPreviouslyStunned, setRedPreviouslyStunned] = useState(false)
+    const [bluePreviouslyStunned, setBluePreviouslyStunned] = useState(false)
 
     const { data: roboticonTickData } = useRoboticonGameStateSubscription()
     let roboticonState: GameState = { 
@@ -34,6 +38,28 @@ export default function SoundPlayer() {
 
     if (previouslyEnabled !== roboticonState.enabled) setPreviouslyEnabled(roboticonState.enabled)
     if (previousTime !== roboticonState.timeLeft) setPreviousTime(roboticonState.timeLeft)
+
+
+
+    const { data: stunData } = useRoboticonStunnedAlliancesSubscription()
+    let stunnedAlliances: StunnedAlliances = {
+        blueIsDisabled: false,
+        blueIsInCooldown: false,
+        redIsDisabled: false,
+        redIsInCooldown: false
+    }
+    if (stunData?.subscribe != null) {
+        stunnedAlliances = JSON.parse(stunData?.subscribe)
+    }
+
+    if ((stunnedAlliances.blueIsDisabled && !bluePreviouslyStunned) || (stunnedAlliances.redIsDisabled && !redPreviouslyStunned)) {
+        stunned.play()
+    }
+
+    if (redPreviouslyStunned !== stunnedAlliances.redIsDisabled) setRedPreviouslyStunned(stunnedAlliances.redIsDisabled);
+    if (bluePreviouslyStunned !== stunnedAlliances.blueIsDisabled) setBluePreviouslyStunned(stunnedAlliances.blueIsDisabled);
+
+
 
     return React.createElement(React.Fragment, null)
 }
